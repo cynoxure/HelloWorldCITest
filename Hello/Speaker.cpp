7d62e20993fc995391451e75e2a9ff9611678223
James Rogers <james.rogers@vethresearch.com>
2020-11-29 21:07:54 -0600

    Add vcpkg

--- Hello/Speaker.cpp
+++ Hello/Speaker.cpp
@@ -1,10 +1,13 @@
 #include "Speaker.h"
+#include <Eigen/Core>
 
 using namespace Hello;
 using namespace std;
+using namespace Eigen;
 
 namespace Hello {
   void Speaker::sayHello() {
+    Vector3d vec;
     cout << "Hello, world!\nDisclaimer: The program uses cppzmq from vcpkg library as its dependencies";
   }
 }
