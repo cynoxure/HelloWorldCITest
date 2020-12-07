695a2f9e443c481da888f6e590e7fa5932a84d93
James Rogers <james.rogers@vethresearch.com>
2020-12-06 21:25:43 -0600

    package error

--- MultiCPackConfig.cmake
+++ MultiCPackConfig.cmake
@@ -2,14 +2,12 @@ include("release/CPackConfig.cmake")
 
 if( WIN32 AND NOT CYGWIN)
     set(CPACK_INSTALL_CMAKE_PROJECTS
-        "debug;vra_navigation_library;ALL;/"
-        "release;vra_navigation_library;ALL;/"
-        "relwithdebinfo;vra_navigation_library;ALL;/"
-        "minsizerel;vra_navigation_library;ALL;/"
+        "debug;HelloWorld;ALL;/"
+        "release;HelloWorld;ALL;/"
         )
 else( )
     set(CPACK_INSTALL_CMAKE_PROJECTS
-        "debug;vra_navigation_library;ALL;/"
-        "release;vra_navigation_library;ALL;/"
+        "debug;HelloWorld;ALL;/"
+        "release;HelloWorld;ALL;/"
         )
 endif( WIN32 AND NOT CYGWIN)
